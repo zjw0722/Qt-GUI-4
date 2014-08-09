16dc22625dfb1d0baa28304a3394c744b62fcd1f
zjw0722 <zjw_0722@163.com>
2014-08-09 17:13:10 +0800

    c

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -5,6 +5,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Dialog w;
+    w.setWindowTitle(QObject::tr("HelloWorld"));
+    w.setMaximumWidth(400);
+    w.setMaximumHeight(300);
+    w.setMinimumWidth(400);
+    w.setMinimumHeight(300);
     w.show();
 
     return a.exec();
